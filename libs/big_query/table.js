6239c8a96f8a5ae8781ca41eaaf0285379f68c52
darthjee <darthjee@gmail.com>
2016-11-04 14:18:58 -0200

    Correct errors

--- libs/big_query/table.js
+++ libs/big_query/table.js
@@ -28,7 +28,7 @@
   };
   
   fn._insertionCallback = function (err, insertErrors, apiResponse, options) {
-    if (err || insertErrror) {
+    if (err || insertErrors) {
       options.error.call(this, err, insertErrors);
     } else {
       options.success.call(this, apiResponse);
